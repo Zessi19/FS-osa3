f7b6a45819ca28ed9bacd964497b010ec252cf1d
Joonas <joonas.jussila@helsinki.fi>
2019-10-14 04:34:03 +0300

    uibuild

---
+++ build/precache-manifest.2a113ceb96cee29a5d83a02839675b62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65debd1975d2c068f5a4a8f645fcde8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa78a599c3a26e789cf",
+    "url": "/static/css/main.abc5b11b.chunk.css"
+  },
+  {
+    "revision": "7bc0652c9b2eb8568d72",
+    "url": "/static/js/2.59f409fb.chunk.js"
+  },
+  {
+    "revision": "2fa78a599c3a26e789cf",
+    "url": "/static/js/main.2e5d72a3.chunk.js"
+  },
+  {
+    "revision": "64fda7fd07fd25d97db7",
+    "url": "/static/js/runtime-main.11bda596.js"
+  }
+]);
--- index.js
+++ index.js
@@ -31,6 +31,7 @@ let persons = [
 // Config
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) { 
   return JSON.stringify(req.body) 
